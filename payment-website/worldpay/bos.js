d9e5dc769949a507ea6b13d43eb6d4b324143464
hhubner <hhubner@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2007-04-14 16:11:17 +0000

    Worldpay payment pages
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@2145 4281704c-cde7-0310-8518-8e2dc76b1ff0

---
+++ payment-website/worldpay/bos.js
@@ -0,0 +1,28 @@
+// ***  preload menue-images *** //
+
+
+
+// ***  extrafenster fuer impressum, kontakt etc. *** //
+var a="x";
+		function window_extra() { 
+				mywin=open(a,"detailwin","width=480,height=600,status=no,toolbar=no,menubar=no,resizable=yes,scrollbars=yes,screenX=100,screenY=100");
+				mywin.moveBy(50,50);
+				mywin.focus();
+		};
+
+		
+		// ***  extrafenster fuer news + archive *** //
+var b="x";
+		function window_news() { 
+				mywin=open(b,"newswin","width=480,height=400,status=no,toolbar=no,menubar=no,resizable=yes,scrollbars=yes,screenX=100,screenY=100");
+				mywin.moveTo(50,50);
+				mywin.focus();
+		};
+
+// ***  extrafenster fuer impressum, kontakt etc. *** //
+var c="x";
+		function window_infosys() { 
+				mywin=open(c,"infowin","width=765,height=500,status=no,toolbar=no,menubar=no,resizable=yes,scrollbars=yes,screenX=50,screenY=50");
+				mywin.moveBy(50,50);
+				mywin.focus();
+		};
