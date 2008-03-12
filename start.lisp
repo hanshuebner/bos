7af30a371c85a01172cf54e5f2aeff72f5cf0669
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-03-12 11:24:37 +0000

    added start.lisp to bos for loading
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@2722 4281704c-cde7-0310-8518-8e2dc76b1ff0

---
+++ start.lisp
@@ -0,0 +1,41 @@
+;;; a quick startup script that can be loaded with all supported lisps
+(in-package :cl-user)
+
+#+cmu(load (compile-file "../../bknr/patches/patch-around-mop-cmucl19a.lisp"))
+
+#+sbcl(require 'asdf)
+
+(load (compile-file "../../thirdparty/asdf/asdf.lisp"))
+
+;;; some helpers
+(defun setup-registry ()
+  (format t "; setting up ASDF registry, please be patient...")
+  (finish-output)
+  (mapc #'(lambda (asd-pathname)
+	    (pushnew (make-pathname :directory (pathname-directory asd-pathname))
+		     asdf:*central-registry*
+		     :test #'equal))
+	(directory #p"../../**/*.asd")))
+
+(defun read-configuration (pathname)
+  (with-open-file (s pathname)
+    (loop for form = (read s nil :end-of-file)
+       while (not (eq form :end-of-file))
+       ;; 2008-03-12 kilian: I have added eval here (e.g. for merge-pathnames) 
+       collect (eval form))))
+
+;;; setup asdf:*central-registry*
+(setup-registry)
+
+;;; load bos project
+(asdf:oos 'asdf:load-op :bos.web)
+(format t "BOS Online-System~%")
+;;; slime
+(asdf:oos 'asdf:load-op :swank)
+(swank-loader::init)                    ; currently necessary
+(swank:create-server :port 4005 :dont-close t)
+;;; start the bos server
+(apply #'bos.m2::reinit (read-configuration "m2.rc"))
+(apply #'bos.web::init (read-configuration "web.rc"))
+(bknr.cron::start-cron)
+
