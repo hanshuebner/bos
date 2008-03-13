e96f6ee02b67b28be0fc6db281aa60e5afa2cfe1
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-03-13 14:42:56 +0000

    make-image-tree now produces first nice output
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@2728 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- web/image-tree.lisp
+++ web/image-tree.lisp
@@ -23,11 +23,17 @@
 	    (store-image-width object)
 	    (store-image-height object))))
 
-(defun make-image-tree-node (image &rest initargs)
-  (make-store-image :image image
-                    :name (image-tree-node-unique-name)
-                    :class-name 'image-tree-node
-                    :initargs initargs))
+(defun make-image-tree-node (image &key geo-rect children)
+  (destructuring-bind (geo-x geo-y geo-width geo-height)
+      geo-rect
+    (make-store-image :image image
+                      :name (image-tree-node-unique-name)
+                      :class-name 'image-tree-node
+                      :initargs `(:geo-x ,geo-x
+                                         :geo-y ,geo-y
+                                         :geo-width ,geo-width
+                                         :geo-height ,geo-height
+                                         :children ,children))))
 
 (defun children-sizes (width height &key (divisor 2))
   (flet ((divide-almost-equally (x)
@@ -42,36 +48,62 @@
           (divide-almost-equally height))))
 
 
+(defun map-children-rects (function left top width-heights)
+  "Calls FUNCTION with (x y width height) for each of the sub-rectangles
+specified by the start point LEFT, TOP and WIDTH-HEIGHTS of the sub-rectangles.
+Collects the results into a list."
+  (let (results)
+    (destructuring-bind (widths heights)
+        width-heights
+      (dolist (w widths (nreverse results))       
+        (let ((safe-top top))           ; pretty ugly, sorry
+          (dolist (h heights)
+            (push (funcall function left safe-top w h) results)
+            (incf safe-top h)))
+        (incf left w)))))
+
+
 (defun make-image-tree (source-image geo-location &key (output-images-size 256))
   (destructuring-bind (geo-x geo-y geo-width geo-height) geo-location
-    (labels ((source-x (x)
-               (- x geo-x))
-             (source-y (y)
-               (- y geo-y))
-             (image-small-enough (image-width image-height)
-               (and (<= image-width output-images-size)
-                    (<= image-height output-images-size)))
-             (%make-image-tree (geo-x geo-y geo-width geo-height image-width image-height)
-               (let ((children (unless (image-small-enough image-width image-height)
-                                 (error "fds"))))
-                 (cl-gd:with-image (image output-images-size output-images-size t)
-                   (cl-gd:copy-image source-image image
-                                     (source-x geo-x) (source-y geo-y) 0 0
-                                     image-width image-height
-                                     :resample t
-                                     :resize t
-                                     :dest-width output-images-size
-                                     :dest-height output-images-size)
-                   (make-image-tree-node image
-                                         :geo-x geo-x
-                                         :geo-y geo-y
-                                         :geo-width geo-width
-                                         :geo-height geo-height
-                                         :children children)))))
-      (with-image-tree-node-counter
-        (%make-image-tree geo-x geo-y geo-width geo-height
-                          (cl-gd:image-width source-image)
-                          (cl-gd:image-height source-image))))))
+    (let* ((source-image-width (cl-gd:image-width source-image))
+           (source-image-height (cl-gd:image-height source-image))
+           (scaler-x (/ source-image-width geo-width))
+           (scaler-y (/ source-image-height geo-height)))
+      (labels ((image-point2geo-point (x y)
+                 (list (+ (/ x scaler-x) geo-x)
+                       (+ (/ y scaler-y) geo-y)))
+               (image-rect2geo-rect (rect)
+                 (destructuring-bind (x y width height)
+                     rect
+                   (let ((x2 (+ x width))
+                         (y2 (+ y height)))
+                     (destructuring-bind (geo-x geo-y)
+                         (image-point2geo-point x y)
+                       (destructuring-bind (geo-x2 geo-y2)
+                           (image-point2geo-point x2 y2)
+                         (list geo-x geo-y (- geo-x2 geo-x) (- geo-y2 geo-y)))))))
+               (image-small-enough (image-width image-height)
+                 (and (<= image-width output-images-size)
+                      (<= image-height output-images-size)))
+               (%make-image-tree (image-x image-y image-width image-height)
+                 (let ((children (unless (image-small-enough image-width image-height)
+                                   (map-children-rects #'%make-image-tree
+                                                       image-x image-y
+                                                       (children-sizes image-width image-height)))))
+                   (cl-gd:with-image (image output-images-size output-images-size t)
+                     (cl-gd:copy-image source-image image
+                                       image-x image-y 0 0
+                                       image-width image-height
+                                       :resample t
+                                       :resize t
+                                       :dest-width output-images-size
+                                       :dest-height output-images-size)
+                     (make-image-tree-node image
+                                           :geo-rect (image-rect2geo-rect
+                                                      (list image-x image-y image-width image-height))
+                                           :children children)))))
+        (with-image-tree-node-counter
+          (%make-image-tree 0 0 source-image-width source-image-height))))))
 
 
 #|
