aaab290d0905155a6c538eabfeedc3acac244e07
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-04-11 11:46:07 +0000

    cleanup projects/bos
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@2959 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-set -x
-(cd ../../thirdparty/cl-gd && make)
-lisp -core cmucl.core -noinit -load load.lisp -load build.lisp
--- hack.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-cd `dirname $0`/..
-lisp -noinit -core bos/build.core -load bos/build.lisp
