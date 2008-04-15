e8ce89c22995d852f30ec9bf497b2b23136db171
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-04-15 13:58:10 +0000

    consistent-p-report in daily.lisp
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@2981 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- web/daily.lisp
+++ web/daily.lisp
@@ -9,4 +9,17 @@
   (snapshot)
   (format t "; rebuilding allocation-cache~%")
   (bos.m2.allocation-cache:rebuild-cache)
+  (format t "; running check (consistent-p)~%")
+  (flet ((consistent-p-report ()
+           (let  (consistent-p)
+             (values (with-output-to-string (*error-output*)
+                       (setq consistent-p (bos.m2::consistent-p)))
+                     consistent-p))))
+    (multiple-value-bind (report consistent-p)
+        (consistent-p-report)
+      (unless consistent-p
+        (bos.m2::send-system-mail :to "kilian.sprotte@gmail.com"
+                                  :subject "(bos.m2::consistent-p) returned NIL"
+                                  :text report))))
   (format t "; done~%"))
+
