b72dd0a790196aaa0bdf8bd66d7f777300069e29
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-04-22 19:31:29 +0000

    some more work on the kml test spider
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3003 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- m2/warm-kml-cache.lisp
+++ m2/warm-kml-cache.lisp
@@ -23,8 +23,9 @@
 
 
 
-
-(defun kml-test (host)
+(defvar *parent-region*)
+(defvar *parent-link*)
+(defun kml-test (host &optional (path "/kml-root"))
   (labels
       ((find-links-in-xml (node)
          (when (listp node)
@@ -47,26 +48,63 @@
             (read-from-string (third (assoc "south" (cxml-xmls:node-children lat-lon-alt-box) :test #'equal)))
             (read-from-string (third (assoc "west" (cxml-xmls:node-children lat-lon-alt-box) :test #'equal)))
             (read-from-string (third (assoc "east" (cxml-xmls:node-children lat-lon-alt-box) :test #'equal))))))
-       (analyze-kml (url)        
+       (region-inside-p (parent region)
+         (destructuring-bind (parent-north parent-south parent-west parent-east) parent
+           (destructuring-bind (region-north region-south region-west region-east) region
+             (and (<= region-north parent-north) ; negative degrees
+                  (>= region-south parent-south) ; negative degrees
+                  (>= region-west parent-west)
+                  (<= region-east parent-east)))))
+       (describe-region-not-inside (parent region)
+         (with-output-to-string (*standard-output*)
+           (destructuring-bind (parent-north parent-south parent-west parent-east) parent
+             (destructuring-bind (region-north region-south region-west region-east) region
+               (when (not (<= region-north parent-north))
+                 (format t "not (<= region-north parent-north)~%")) 
+               (when (not (>= region-south parent-south))
+                 (format t "not (>= region-south parent-south)~%"))
+               (when (not (>= region-west parent-west))
+                 (format t "not (>= region-west parent-west)~%"))
+               (when (not (<= region-east parent-east))
+                 (format t "not (<= region-east parent-east)~%"))))))
+       (analyze-kml (url &optional expected-region)
+         (print url)
          (multiple-value-bind (content status-code)
              (drakma:http-request (format nil "~A?lang=de" url))
            (declare (ignore status-code))
-           (analyze-node (cxml:parse content (cxml-xmls:make-xmls-builder)))))
+           (let ((*parent-link* url))
+             (analyze-node (cxml:parse content (cxml-xmls:make-xmls-builder)) expected-region))))
        (analyze-children (node)
          (mapc #'analyze-node (cxml-xmls:node-children node)))
-       (analyze-node (node)
-         (when (listp node)
-           (print (cxml-xmls:node-name node))         
+       (analyze-node (node &optional expected-region)
+         (when (listp node)         
+           (when expected-region
+             (assert (equal expected-region (lispify-region (find-child "Region" node)))))
+           ;; (print (cxml-xmls:node-name node))           
            (arnesi:switch ((cxml-xmls:node-name node) :test #'equal)
              ("NetworkLink"
               (let ((region (find-child "Region" node))
                     (link (find-child "Link" node)))
                 (assert region)
-                (print (lispify-region region))))
+                (assert link)
+                (print (lispify-region region))
+                (print 'region-inside-p)
+                (assert (region-inside-p *parent-region* (lispify-region region)) nil
+                        "region of ~s is not inside region of ~s~%~S~%~S~%REASON:~%~A" (third (find-child "href" link)) *parent-link*
+                        (lispify-region region) *parent-region*
+                        (describe-region-not-inside *parent-region* (lispify-region region)))
+                (analyze-kml (third (find-child "href" link)) (lispify-region region))))
+             ("kml"
+              (print 'hu)
+              (let ((region (find-child "Region" node)))
+                (assert region)
+                (let ((*parent-region* (lispify-region region)))
+                  (analyze-children node))))
              (t (analyze-children node))))))
     (analyze-kml (make-instance 'puri:uri
                                 :scheme :http
                                 :host host
-                                :path "/kml-root"))))
+                                :path path))
+    t))
 
 
