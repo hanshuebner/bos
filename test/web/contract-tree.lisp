dc84f0792685e972f0cbca909e27a35827b70c46
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-05-20 11:28:26 +0000

    calling test suite now :bos.test.web.quad-tree
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3140 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- test/web/contract-tree.lisp
+++ test/web/contract-tree.lisp
@@ -1,8 +1,8 @@
 (in-package :bos.test)
-(in-suite :bos.test.web.contract-tree)
+(in-suite :bos.test.web.quad-tree)
 
 (test geo-box-intersect-p.1
-  (let ((tree (make-instance 'bos.web::quad-tree-node :geo-box bos.web::*m2-geo-box*)))
+  (let ((tree (make-instance 'bos.web::quad-node :geo-box bos.web::*m2-geo-box*)))
     (bos.web::ensure-child tree 0)
     (bos.web::ensure-child tree 1)
     (bos.web::ensure-child tree 2)
@@ -22,7 +22,7 @@
              (let ((count 0))
                (bos.web::map-nodes (lambda (node) (declare (ignore node)) (incf count)) tree)
                count)))
-    (let ((tree (make-instance 'bos.web::quad-tree-node :geo-box bos.web::*m2-geo-box*)))
+    (let ((tree (make-instance 'bos.web::quad-node :geo-box bos.web::*m2-geo-box*)))
       (is (= 1 (count-nodes tree)))
       (bos.web::ensure-node-with-path tree '(0))
       (is (= 2 (count-nodes tree)))
@@ -38,12 +38,12 @@
       (is (= 7 (count-nodes tree))))))
 
 (test node-path.1
-  (let ((tree (make-instance 'bos.web::quad-tree-node :geo-box bos.web::*m2-geo-box*)))
+  (let ((tree (make-instance 'bos.web::quad-node :geo-box bos.web::*m2-geo-box*)))
     (for-all ((path (gen-list :elements (gen-integer :min 0 :max 3))))
       (is (equal path (bos.web::node-path tree (bos.web::ensure-node-with-path tree path)))))))
 
 (test geo-point-in-box-p
-  (let* ((tree (make-instance 'bos.web::quad-tree-node :geo-box bos.web::*m2-geo-box*))
+  (let* ((tree (make-instance 'bos.web::quad-node :geo-box bos.web::*m2-geo-box*))
          (box (bos.web::geo-box tree)))
     (is-true (bos.web::geo-point-in-box-p box (list (bos.web::geo-box-west box) (bos.web::geo-box-north box))))
     (is-false (bos.web::geo-point-in-box-p box (list (bos.web::geo-box-east box) (bos.web::geo-box-north box))))
