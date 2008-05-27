5c394f6d1f14b081ecf32e91c1f27511678a9b93
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-05-27 12:48:28 +0000

    some changes to make-sat-layer
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3197 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- web/sat-tree.lisp
+++ web/sat-tree.lisp
@@ -3,15 +3,13 @@
 (defclass sat-node (node-extension)
   ((image :accessor image :initarg :image :type store-image)))
 
-(defmethod leaf-node-p ((node sat-node))
-  t)                                    ; for now
-
 (defpersistent-class sat-layer ()
   ((name :reader name :initarg :name)))
 
 (defpersistent-class sat-image (store-image)
   ((layer :reader layer :initarg :layer)
-   (node :reader node :initarg :node)
+   (node :reader node :initarg :node :transient t)
+   (path :reader path :initarg :path)
    (image-geo-box :accessor image-geo-box :initarg :image-geo-box
                   :type geo-box
                   :documentation "can be different from base-node's geo-box")))
@@ -22,11 +20,27 @@
 (defmethod name ((obj sat-image))
   (name (layer obj)))
 
-(defun make-sat-layer (image geo-box name &optional (start-depth 0))
-  (check-type image cl-gd::image)
-  (assert (geo-box-encloses-p *m2-geo-box* geo-box))
-  (check-type name symbol)
-  (assert (not (find name (class-instances 'sat-layer) :key #'name)))
-  (check-type start-depth (integer 0))
-  )
+;;; was passiert, wenn die tile intersections sehr klein (sozusagen
+;;; Rundungsfehler) sind
+(defun make-sat-layer (image geo-box name &optional (start-depth 0))  
+  (labels ((layer-quad-nodes ()
+             (let (nodes)
+               (ensure-intersecting-children *quad-tree* geo-box
+                                             (lambda (n) (when (= start-depth (depth n))
+                                                           (push n nodes)))
+                                             (lambda (n) (= start-depth (depth n))))
+               nodes)))
+    (check-type image cl-gd::image)
+    (assert (geo-box-encloses-p *m2-geo-box* geo-box))
+    (check-type name symbol)
+    (assert (not (find name (class-instances 'sat-layer) :key #'name)))
+    (check-type start-depth (integer 0))
+    (let ((layer-quad-nodes (layer-quad-nodes)))
+      layer-quad-nodes)))
+
+;; (with-store-image (image (first (class-instances 'store-image)))
+;;   (make-sat-layer image
+;;                   (rectangle-geo-box (make-rectangle :x 5400 :y 5400 :width 2000 :height 2000))
+;;                   :sat1
+;;                   3))
 
