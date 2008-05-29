b9d6d5e83ea4fa072041a6cd0c99297e05c9d0cf
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-05-29 15:59:31 +0000

    make-sat-image-tile now allows for creation of a single level of sat tiles. Still need to work on persistence.
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3204 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- web/sat-tree.lisp
+++ web/sat-tree.lisp
@@ -4,7 +4,9 @@
   ((image :accessor image :initarg :image :type store-image)))
 
 (defpersistent-class sat-layer ()
-  ((name :reader name :initarg :name)))
+  ((name :reader name :initarg :name
+         :index-type unique-index
+	 :index-reader find-sat-layer)))
 
 (defpersistent-class sat-image (store-image)
   ((layer :reader layer :initarg :layer)
@@ -20,23 +22,96 @@
 (defmethod name ((obj sat-image))
   (name (layer obj)))
 
-;;; was passiert, wenn die tile intersections sehr klein (sozusagen
-;;; Rundungsfehler) sind
+(defconstant +max-sat-image-tile-pixel-area+ (float (expt 256 2) 0d0))
+
+(defun sat-image-tile-properties (image geo-box tile-geo-box &optional scaling)
+  #+nil(declare (optimize speed))
+  ;; (the (double-float 0d0 #.(float most-positive-fixnum 0d0)) ...)
+  ;; might be useful
+  (let* ((gw (float (the (integer 1 #.most-positive-fixnum) (cl-gd:image-width image)) 0d0))
+         (gh (float (the (integer 1 #.most-positive-fixnum) (cl-gd:image-height image)) 0d0))
+         (w (geo-box-west geo-box))
+         (n (geo-box-north geo-box))
+         (e (geo-box-east geo-box))
+         (s (geo-box-south geo-box))        
+         (bw (geo-box-west tile-geo-box))
+         (bn (geo-box-north tile-geo-box))
+         (be (geo-box-east tile-geo-box))
+         (bs (geo-box-south tile-geo-box))
+         (xu (/ (- e w) gw))
+         (yu (/ (- n s) gh))
+         (px (floor (/ (- bw w) xu)))
+         (py (floor (/ (- n bn) yu)))
+         (px2 (ceiling (/ (- be w) xu)))
+         (py2 (ceiling (/ (- n bs) yu)))
+         (pw (- px2 px))
+         (ph (- py2 py))
+         (rounded-geo-box (make-geo-box (+   (* px  xu) w)
+                                        (- n (* py  yu))
+                                        (+   (* px2 xu) w)
+                                        (- n (* py2 yu))))
+         (scaling (if scaling
+                      scaling
+                      (ceiling (sqrt (/ (* pw ph) +max-sat-image-tile-pixel-area+)))))
+         (tw (round (/ pw scaling)))
+         (th (round (/ ph scaling))))
+    (values scaling
+            pw ph px py px2 py2 
+            tw th rounded-geo-box)))
+
+(defun make-sat-image-tile (image geo-box quad-node tile-geo-box name max-scaling)
+  (multiple-value-bind (scaling
+                        pw ph px py px2 py2 
+                        tw th rounded-geo-box)
+      (sat-image-tile-properties image geo-box tile-geo-box max-scaling)
+    (declare (ignore scaling px2 py2))
+    (let ((path (node-path quad-node)))
+      (cl-gd:with-image (cl-gd:*default-image* tw th t)
+        (cl-gd:copy-image image cl-gd:*default-image*
+                          px py 0 0
+                          pw ph
+                          :resize t :resample t
+                          :dest-width tw :dest-height th)
+        (make-instance 'node-extension
+                       :name name
+                       :base-node quad-node
+                       :image (make-store-image :class-name 'sat-image
+                                                :name (format nil "~A-~{~D~}" name path)
+                                                :initargs `(:path ,path
+                                                                  :image-geo-box ,rounded-geo-box)))))))
+
 (defun make-sat-layer (image geo-box name &optional (start-depth 0))  
+  (check-type name symbol)
+  (assert (not (find-sat-layer name)))
+  (check-type image cl-gd::image)
+  (assert (geo-box-encloses-p *m2-geo-box* geo-box))  
+  (check-type start-depth (integer 0))
   (labels ((layer-quad-nodes ()
              (let (nodes)
                (ensure-intersecting-children *quad-tree* geo-box
                                              (lambda (n) (when (= start-depth (depth n))
                                                            (push n nodes)))
-                                             (lambda (n) (= start-depth (depth n))))
-               nodes)))
-    (check-type image cl-gd::image)
-    (assert (geo-box-encloses-p *m2-geo-box* geo-box))
-    (check-type name symbol)
-    (assert (not (find name (class-instances 'sat-layer) :key #'name)))
-    (check-type start-depth (integer 0))
-    (let ((layer-quad-nodes (layer-quad-nodes)))
-      layer-quad-nodes)))
+                                             (lambda (n) (= start-depth (depth n))))               
+               (mapcar
+                (lambda (quad-node)
+                  (list quad-node (geo-box-intersection geo-box (geo-box quad-node))))
+                nodes)))
+           (quad-node (node) (first node))
+           (tile-geo-box (node) (second node))
+           (pw-ph-large-enough (node)
+             (multiple-value-bind (scaling pw ph)
+                 (sat-image-tile-properties image geo-box (tile-geo-box node))
+               (declare (ignore scaling))
+               (and (> pw 1) (> ph 1))))
+           (max-scaling (nodes)
+             (reduce #'max nodes
+                     :key (lambda (node)
+                            (sat-image-tile-properties image geo-box (tile-geo-box node))))))    
+    (let* ((nodes (remove-if-not #'pw-ph-large-enough (layer-quad-nodes)))           
+           (max-scaling (max-scaling nodes)))
+      (dolist (node nodes)
+        (make-sat-image-tile image geo-box (quad-node node)
+                             (tile-geo-box node) name max-scaling)))))
 
 ;; (with-store-image (image (first (class-instances 'store-image)))
 ;;   (make-sat-layer image
