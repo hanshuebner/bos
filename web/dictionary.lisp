f85d52808eee8399e9ff4c1d1b1da2aacda5a54a
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-06-17 10:21:50 +0000

    dictionary: removed website-language dependency
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3285 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- web/dictionary.lisp
+++ web/dictionary.lisp
@@ -1,42 +1,69 @@
 (in-package :bos.web)
 
 (defvar *dictionary* (make-hash-table :test #'equal))
-(defvar *dictionary-last-read* 0)
+(defvar *dictionary-last-reads-alist* nil)
 
-(defmethod dictionary-entry ((key string) (language website-language))
-  (load-dictionary-if-needed)
-  (cdr (assoc language (gethash key *dictionary*))))
+(defparameter *dictionary-directory* *website-directory*
+  "The xml files containing dictionary definitions for a
+  particular language are stored under
+  *dictionary-directory*/<language>/dictionary.xml.")
 
-(defmethod dictionary-entry ((key string) (code string))
-  (assert (language-with-code code) (code) "language with code ~s does not exist" code)
-  (dictionary-entry key (language-with-code code)))
+(deftype dictionary-language ()
+  'keyword)
 
-(defmethod (setf dictionary-entry) ((value string) (key string) (language website-language))  
-  (let ((it (assoc language (gethash key *dictionary*))))
+(deftype dictionary-language-designator ()
+  '(or string dictionary-language))
+
+(defun dictionary-language (language-designator)
+  (declare (dictionary-language-designator language-designator))
+  (typecase language-designator
+    (string (intern (string-upcase language-designator) #.(find-package "KEYWORD")))
+    (keyword language-designator)))
+
+(defun dictionary-entry (key language)  
+  (let ((language (dictionary-language language)))
+    (load-dictionary-if-needed language)
+    (or (cdr (assoc language (gethash key *dictionary*)))
+        key)))
+
+(defun (setf dictionary-entry) (value key language)  
+  (let* ((language (dictionary-language language))
+         (it (assoc language (gethash key *dictionary*))))
     (if it
         (rplacd it value)
         (push (cons language value) (gethash key *dictionary*)))))
 
+(defun dictionary-clear-entries-by-language (language)
+  (declare (dictionary-language language))
+  (maphash (lambda (key value)             
+             ;; (setf gethash) with key explicitly allowed by ANSI CL
+             (setf (gethash key *dictionary*)
+                   (remove language value :key #'car)))
+           *dictionary*))
+
+(defun dictionary-last-read (language)
+  (declare (dictionary-language language))
+  (let ((time (cdr (assoc language *dictionary-last-reads-alist*))))
+    (if time
+        time
+        0)))
+
+(defun (setf dictionary-last-read) (value language)
+  (declare (dictionary-language language))
+  (let ((cons (assoc language *dictionary-last-reads-alist*)))
+    (if cons
+        (rplacd cons value)
+        (push (cons language value) *dictionary-last-reads-alist*))))
+
 (defun dictionary-xml-path (language)
+  (declare (dictionary-language language))
   (merge-pathnames (make-pathname :name "dictionary"
                                   :type "xml"
-                                  :directory (list :relative "templates" (website-language-code language)))
-                   *website-directory*))
+                                  :directory (list :relative "templates" (string-downcase (string language))))
+                   *dictionary-directory*))
 
-(defun dictionary-xml-files-last-write-date (xml-paths)
-  (handler-case
-      (reduce #'max xml-paths :key #'file-write-date)
-    (error (c)
-      (cond
-        ((not (every #'probe-file xml-paths))
-         (error "dictionary xml file does not exist:~%~a"
-                (find-if-not #'probe-file xml-paths)))
-        ((not (every #'file-write-date xml-paths))
-         (error "cannot determine file-write-date of:~%~a"
-                (find-if-not #'file-write-date xml-paths)))
-        (t (error c))))))
-
-(defun load-dictionary (languages xml-paths)
+(defun load-dictionary (language xml-path)
+  (declare (dictionary-language language))
   (labels ((load-language (language xml-path)             
              (handler-case
                  (let ((xmls (cxml:parse-file xml-path (cxml-xmls:make-xmls-builder))))
@@ -45,21 +72,22 @@
                    (dolist (element (cxml-xmls:node-children xmls))
                      (when (consp element)
                        (let ((key (cxml-xmls:node-name element))
-                             (value (first (cxml-xmls:node-children element))))
-                         (assert (stringp value))
-                         (setf (dictionary-entry key language) value)))))
+                             (value (first (cxml-xmls:node-children element))))                         
+                         (assert (or (null value) (stringp value)))
+                         (when value
+                           (setf (dictionary-entry key language) value))))))
                (error (c)
                  (error "Error while loading ~a:~%~a"
-                        (enough-namestring xml-path *website-directory*) c)))))
-    (clrhash *dictionary*)
-    (mapc #'load-language languages xml-paths)
-    (setf *dictionary-last-read* (get-universal-time))
+                        (enough-namestring xml-path *dictionary-directory*) c)))))    
+    (dictionary-clear-entries-by-language language)
+    (load-language language xml-path)    
+    (setf (dictionary-last-read language) (get-universal-time))
     *dictionary*))
 
-(defun load-dictionary-if-needed ()
-  (let* ((languages (class-instances 'website-language))
-         (xml-paths (mapcar #'dictionary-xml-path languages)))    
-    (when (> (dictionary-xml-files-last-write-date xml-paths)
-             *dictionary-last-read*)
-      (load-dictionary languages xml-paths))))
+(defun load-dictionary-if-needed (language)  
+  (declare (dictionary-language language))
+  (let ((xml-path (dictionary-xml-path language)))
+    (when (> (file-write-date xml-path)
+             (dictionary-last-read language))
+      (load-dictionary language xml-path))))
 
