87fd2e8c6d274de7d8c33d91cd6075401706f0a3
ksprotte <ksprotte@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-09-09 15:50:25 +0000

    cleanup poi/functions.js
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3869 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- payment-website/static/poi/functions.js
+++ payment-website/static/poi/functions.js
@@ -1,59 +1,59 @@
 // JavaScript Document
-		//
-		//  In my case I want to load them onload, this is how you do it!
-		// 
+//
+//  In my case I want to load them onload, this is how you do it!
+//
 
 Event.observe(window, 'load', loadAccordions, false);
-		//
-		//	Set up all accordions
-		//
+//
+//      Set up all accordions
+//
 function loadAccordions() {
 
-			
-			var mainAccordion = new accordion('content_main',{ 
-		    resizeSpeed : 15,
-			
-			  defaultSize : {
-                  height:75
-               }
-			});
-			
-/*			var VerticalAccordion = new accordion('vertical_container', {
 
-			  classNames : {
-					toggle : 'vertical_accordion_toggle',
-					toggleActive : 'vertical_accordion_toggle_active',
-					content : 'vertical_accordion_content'
-				}
-			});*/
-			
-			// Open first one
-			mainAccordion.activate($$('#content_main .accordion_toggle')[0]);
-			//VerticalAccordion.activate($$('#vertical_container .vertical_accordion_toggle')[0]);
-			// Special thanks go out to Will Shaver @ http://primedigit.com/
+    var mainAccordion = new accordion('content_main',{
+            resizeSpeed : 15,
 
-/*var verticalAccordions = $$('.accordion_toggle');
-verticalAccordions.each(function(accordion) {
-    $(accordion.next(0)).setStyle(
-	{
-        height: '0px'}
-		);
-});*/
+            defaultSize : {
+                height:75
+            }
+        });
+
+    //     var VerticalAccordion = new accordion('vertical_container', {
+
+    //             classNames : {
+    //                 toggle : 'vertical_accordion_toggle',
+    //                 toggleActive : 'vertical_accordion_toggle_active',
+    //                 content : 'vertical_accordion_content'
+    //             }
+    //         });
+
+    // Open first one
+    mainAccordion.activate($$('#content_main .accordion_toggle')[0]);
+
+    //VerticalAccordion.activate($$('#vertical_container .vertical_accordion_toggle')[0]);
+    // Special thanks go out to Will Shaver @ http://primedigit.com/
+
+    //     var verticalAccordions = $$('.accordion_toggle');
+    //     verticalAccordions.each(function(accordion) {
+    //             $(accordion.next(0)).setStyle(
+    //                                           {
+    //                                               height: '0px'}
+    //                                           );
+    //         });
 
 }
 
-	// remove the registerOverlay call to disable the controlbar
-	hs.registerOverlay(
-    	{
-    		thumbnailId: null,
-    		overlayId: 'controlbar',
-    		position: 'top right',
-    		hideOnMouseOut: true
-		}
-	);
-	
-    hs.graphicsDir = '/static/poi/graphics/';
-    hs.outlineType = 'rounded-white';
-    // Tell Highslide to use the thumbnail's title for captions
-    //hs.captionEval = 'this.thumb.title';
+// remove the registerOverlay call to disable the controlbar
+hs.registerOverlay(
+                   {
+                       thumbnailId: null,
+                           overlayId: 'controlbar',
+                           position: 'top right',
+                           hideOnMouseOut: true
+                           }
+                   );
 
+hs.graphicsDir = '/static/poi/graphics/';
+hs.outlineType = 'rounded-white';
+// Tell Highslide to use the thumbnail's title for captions
+//hs.captionEval = 'this.thumb.title';
