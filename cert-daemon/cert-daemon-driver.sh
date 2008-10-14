acac01f9738df16d0a0e9f75e1a409f1888f1fcb
hans <hans@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-10-14 20:14:36 +0000

    Put some path names in.
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@3991 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- cert-daemon/cert-daemon-driver.sh
+++ cert-daemon/cert-daemon-driver.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 export PATH=/bin:/usr/bin:/usr/local/bin
-HOME=/home/bknr/certs
+CERTS=/home/bknr/certs
+CHECKOUT=/home/bknr/bknr-svn/projects/bos
 hostname=`hostname`
 
 cd $HOME
 
 while true
 do
-    sh gen-cert.sh
+    sh $CHECKOUT/gen-cert.sh
     mail -s "Cert daemon on $hostname crashed" hans < /dev/null
     sleep 5
 done
