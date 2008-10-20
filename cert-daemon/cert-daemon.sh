0731b16867fb989ca37d9e4824582c54e98133d4
hans <hans@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2008-10-20 21:29:03 +0000

    flatten is not a variable, but a pdftk command
    git-svn-id: svn://bknr.net/svn/trunk/projects/bos@4009 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- cert-daemon/cert-daemon.sh
+++ cert-daemon/cert-daemon.sh
@@ -18,11 +18,11 @@ gen_certs()
     download_pdf_file=download-spool/$contract.pdf
 
     if [ $language = de -a -f $print_fdf_file ]; then
-        pdftk $print_m2s_pdf_file fill_form $print_fdf_file output $print_pdf_file $flatten
+        pdftk $print_m2s_pdf_file fill_form $print_fdf_file output $print_pdf_file flatten
         echo generated $print_pdf_file
     fi
 
-    pdftk $download_m2s_pdf_file fill_form $download_fdf_file output $download_pdf_file $flatten
+    pdftk $download_m2s_pdf_file fill_form $download_fdf_file output $download_pdf_file flatten
     echo generated $download_pdf_file
     rm -f $print_m2s_pdf_file $print_fdf_file $download_m2s_pdf_file $download_fdf_file
     trap "" EXIT
