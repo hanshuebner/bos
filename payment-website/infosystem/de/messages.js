1cf4e706b1be1bc31c69cfe5d2ac5c7df708858a
Hans Huebner <hans.huebner@gmail.com>
2012-02-22 12:56:46 +0100

    textual edits

--- payment-website/infosystem/de/messages.js
+++ payment-website/infosystem/de/messages.js
@@ -1,10 +1,10 @@
 
 document.messages = {
 	'Anzahl Sponsoren': 'Anzahl Sponsoren',
-	'Anzahl verkaufte m²': 'Anzahl verkaufte m²',
+	'Anzahl verkaufte m²': 'Anzahl gesponserte m²',
 	'Das Laden des Panoramas dauert einen Moment und benötigt Java in Ihrem Browser.<br /><br />Klicken und Ziehen Sie mit der Maus, um sich im Panorama umzusehen!': 'Das Laden des Panoramas dauert einen Moment und benötigt Java in Ihrem Browser.<br /><br />Klicken und Ziehen Sie mit der Maus, um sich im Panorama umzusehen!',
 	'Diese Anwendung benötigt Cookies, um zu funktionieren.  Bitte schalten Sie Cookies in Ihrem Browser ein.': 'Diese Anwendung benötigt Cookies, um zu funktionieren.  Bitte schalten Sie Cookies in Ihrem Browser ein.',
-	'Dieser m² wurde bisher noch nicht verkauft': 'Dieser m² wurde bisher noch nicht verkauft',
+	'Dieser m² wurde bisher noch nicht verkauft': 'Dieser m² wurde bisher noch nicht gesponsert',
 	'Fehler beim Laden der POI-Informationen, bitte probieren Sie es später noch einmal': 'Fehler beim Laden der POI-Informationen, bitte probieren Sie es später noch einmal',
 	'Land': 'Land',
 	'Luftbild': 'Luftbild',
@@ -12,12 +12,12 @@ document.messages = {
 	'Panorama': 'Panorama',
 	'Sat-Karte': 'Sat-Karte',
 	'Sponsor-ID': 'Sponsor-ID',
-	'Verkaufte m²': 'Verkaufte m²',
-	'Zu Verkaufen': 'Zu Verkaufen',
-	'gesponsort': 'gesponsort',
+	'Verkaufte m²': 'Gesponsorte m²',
+	'Zu Verkaufen': 'Noch zu haben',
+	'gesponsort': 'gesponsert',
 	'meine m²': 'meine m²',
-	'm²-Verkaufsgebiet': 'm²-Verkaufsgebiet',
-	'noch nicht verkauft': 'noch nicht verkauft',
+	'm²-Verkaufsgebiet': 'm²-Sponsorgebiet',
+	'noch nicht verkauft': 'noch nicht gesponsert',
 	'seit': 'seit',
-	'zu verkaufen!': 'zu verkaufen!'
+	'zu verkaufen!': 'zu haben!'
 };
--- payment-website/infosystem/en/messages.js
+++ payment-website/infosystem/en/messages.js
@@ -1,10 +1,10 @@
 
 document.messages = {
 	'Anzahl Sponsoren': 'Number of sponsors',
-	'Anzahl verkaufte m²': 'Number of sold m²',
+	'Anzahl verkaufte m²': 'Number of sponsored m²',
 	'Das Laden des Panoramas dauert einen Moment und benötigt Java in Ihrem Browser.<br /><br />Klicken und Ziehen Sie mit der Maus, um sich im Panorama umzusehen!': 'Loading the panoramic view takes a while and requires Java<br /><br />Click and drag with your mouse in order to look around in the image',
 	'Diese Anwendung benötigt Cookies, um zu funktionieren.  Bitte schalten Sie Cookies in Ihrem Browser ein.': 'This application requires that cookies are enabled in your browser.  Please enable Cookies and try again!',
-	'Dieser m² wurde bisher noch nicht verkauft': 'This m² has not yet been sold.',
+	'Dieser m² wurde bisher noch nicht verkauft': 'This m² has not yet been sponsored.',
 	'Fehler beim Laden der POI-Informationen, bitte probieren Sie es später noch einmal': 'Error loading POI information, please try again later!',
 	'Land': 'Country',
 	'Luftbild': 'Airal picture',
@@ -12,12 +12,12 @@ document.messages = {
 	'Panorama': 'Panoramic view',
 	'Sat-Karte': 'Sat image',
 	'Sponsor-ID': 'Donor ID',
-	'Verkaufte m²': 'Sold m²',
-	'Zu Verkaufen': 'For sale!',
+	'Verkaufte m²': 'Sponsored m²',
+	'Zu Verkaufen': 'Not claimed!',
 	'gesponsort': 'donated',
 	'meine m²': 'my m²',
 	'm²-Verkaufsgebiet': 'm² sale area',
-	'noch nicht verkauft': 'not yet sold',
+	'noch nicht verkauft': 'not yet claimed',
 	'seit': 'since',
-	'zu verkaufen!': 'For sale!'
+	'zu verkaufen!': 'Not claimed!'
 };
